eb4bb46fdc1d042f7f86f266bfe393bbe5a8eaca
Ajay <sumitsrawat2003@gmail.com>
2024-05-04 10:33:23 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import ChatScreen from "./ChatScreen";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Start from "./Start";
+
+const Stack = createNativeStackNavigator();
+function App(){
+  return(
+    <NavigationContainer>
+    <Stack.Navigator
+    >
+       <Stack.Screen name="start" component={Start}  options={{ headerShown: false }}/>
+                <Stack.Screen 
+      options={{ headerShown: false ,headerStyle:{
+        backgroundColor:'#0000000'
+      }}}
+      name="chat" component={ChatScreen} />
+      
+  
+        
+
+      
+    </Stack.Navigator>
+  </NavigationContainer>
+
+  )
+}
+export default App;
